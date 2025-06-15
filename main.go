14689e7b51baea161f5a57f1eb2c9caa8cb52ce4
alexprimak24 <aprimak43@gmail.com>
2025-06-15 09:54:07 +0000

    finished the task to receive the status of the health of the server

--- main.go
+++ main.go
@@ -5,9 +5,17 @@ import "net/http"
 func main() {
 	mux := http.NewServeMux()
 
-	// use .Handle to register the custom handler for '/'
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	// Serve static file users /app/ by stripping prefix
+	// before handling it to the file server
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
 
+	// Register a different handler for the root path
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 	// Struct that describes a server config
 	server := &http.Server{
 		Addr: ":8080", // listen to port 8080
